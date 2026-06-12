0bd1361130b9b0ac9b24d85a6ff03143849aab66
Lana Shotashvili <shotashvililana@gmail.com>
2026-06-12 15:08:08 +0400

    feat: connect input fields to react-hook-form and add watch variables

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -2,20 +2,49 @@ import { useForm } from "react-hook-form";
 import "./Form.css";
 
 function Form() {
-  const { register, handleSubmit, formState = { errors } } = useForm();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  const nameValue = watch("name");
+  const usernameValue = watch("username");
+  const emailValue = watch("email");
+  const phoneValue = watch("phone");
 
   return (
     <div className="form-container">
       <h1>აგენტის დამატება</h1>
-      <form className="form">
+
+      <form className="form" onSubmit={handleSubmit(onSubmit)}>
         <div className="form-group">
-          <input type="text" placeholder="სახელი" />
-          <input type="text" placeholder="გვარი" />
+          <div className="field">
+            <input type="text" placeholder="სახელი" {...register("name")} />
+          </div>
+
+          <div className="field">
+            <input type="text" placeholder="გვარი" {...register("username")} />
+          </div>
         </div>
 
         <div className="form-group">
-          <input type="email" placeholder="ელ-ფოსტა" />
-          <input type="text" placeholder="ტელეფონის ნომერი" />
+          <div className="field">
+            <input type="email" placeholder="ელ-ფოსტა" {...register("email")} />
+          </div>
+
+          <div className="field">
+            <input
+              type="text"
+              placeholder="ტელეფონის ნომერი"
+              {...register("phone")}
+            />
+          </div>
         </div>
       </form>
     </div>
